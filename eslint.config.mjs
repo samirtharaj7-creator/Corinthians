808f4970a2df18cdc5cc66f9323d27de14efceed
samirtharaj7-creator <samirtharaj.7@gmail.com>
2026-07-18 19:28:05 +0530

    Exclude generated deployment output from lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = [...nextVitals, ...nextTs];
+const eslintConfig = [
+  { ignores: ["outputs/corinthians-deployment/**"] },
+  ...nextVitals,
+  ...nextTs,
+];
 
 export default eslintConfig;
